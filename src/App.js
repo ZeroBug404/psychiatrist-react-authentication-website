0053d047bbe27ec1c8e689d4e58c77650c1c76c8
ZeroBug <404.zerobug@gmail.com>
2022-04-16 22:34:02 +0600

    Header Footer component added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Footer from './pages/Shared/Footer/Footer';
+import Header from './pages/Shared/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
